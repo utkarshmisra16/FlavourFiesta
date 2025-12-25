0b395413a93d99b9e5828ecaad889f1f4e5e6f22
Utkarsh Misra <109430518+utkarshmisra16@users.noreply.github.com>
2025-12-25 19:42:36 +0000

    Setting Breakfast Page layout.

--- assets/js/global.js
+++ assets/js/global.js
@@ -5,3 +5,15 @@
  */
 
 "use strict";
+
+/**
+ * Adds event to multiple elements
+ * @param {NodeList} $elements NodeList
+ * @param {String} eventType Event type string
+ * @param {Function} callback Callback function
+ */
+window.addEventOnElements = ($elements, eventType, callback) => {
+    for (const $element of $elements){
+        $element.addEventListener(eventType, callback);
+    }
+}
--- assets/js/home.js
+++ assets/js/home.js
@@ -5,3 +5,60 @@
  */
 
 "use strict";
+
+// Home Page Search
+const /** {NodeElement} */ $searchField = document.querySelector('[data-search-field]');
+const /** {NodeElement} */ $searchBtn = document.querySelector('[data-search-btn]');
+
+$searchBtn.addEventListener('click', function () {
+    if($searchField.value) window.location = `/recipes.html?q=${$searchField.value}`;
+});
+
+// Search submit when press "Enter" key
+$searchField.addEventListener('keydown', e => {
+    if (e.key === 'Enter') $searchBtn.click();
+});
+
+// Tab Panel Navigation
+const /** {NodeList} */ $tabBtns = document.querySelectorAll('[data-tab-btn]');
+const /** {NodeList} */ $tabPanels = document.querySelectorAll('[data-tab-panel]');
+
+let /** {Node Element} */ [$lastActiveTabPanel] = $tabPanels;
+let /** {Node Element} */ [$lastActiveTabBtn] = $tabBtns;
+
+addEventOnElements($tabBtns, 'click', function () {
+    $lastActiveTabPanel.setAttribute("hidden", "");
+    $lastActiveTabBtn.setAttribute("aria-selected", false);
+    $lastActiveTabBtn.setAttribute("tabindex", -1);
+
+    const /** {Node Element} */ $currentTabPanel = document.querySelector(`#${this.getAttribute("aria-controls")}`);
+    $currentTabPanel.removeAttribute("hidden");
+    this.setAttribute("aria-selected", true);
+    this.setAttribute("tabindex", 0);
+
+    $lastActiveTabPanel = $currentTabPanel;
+    $lastActiveTabBtn = this;
+});
+
+// Navigate Tab with arrow key
+addEventOnElements($tabBtns, 'keydown', function (e) {
+    const /** {NodeElement} */ $nextElement = this.nextElementSibling;
+    const /** {NodeElement} */ $previousElement = this.previousElementSibling;
+
+    if(e.key === "ArrowRight" && $nextElement){
+        this.setAttribute("tabindex", -1);
+        $nextElement.setAttribute("tabindex", 0);
+        $nextElement.focus();
+    } 
+    else if(e.key === "ArrowLeft" && $previousElement){
+        this.setAttribute("tabindex", -1);
+        $previousElement.setAttribute("tabindex", 0);
+        $previousElement.focus();
+    }
+    else if(e.key === "Tab"){
+        this.setAttribute("tabindex", -1);
+        $lastActiveTabBtn.setAttribute("tabindex", 0);
+        
+    }
+});
+    let /** {Number} */ index = Array.prototype.indexOf.call($tabBtns, this);
